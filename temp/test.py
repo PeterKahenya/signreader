6d33aec205292d1c802649d9672271d4f8384a2e
PeterKahenya <kahenya0@gmail.com>
2019-06-08 00:35:51 +0300

    Finished with letter a

--- temp/test.py
+++ temp/test.py
@@ -1,24 +1,11 @@
+import cv2
 import os
-import numpy as np
-import tensorflow.keras as keras
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
 
-WORK_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MODEL_DIR=os.path.join(os.path.join(WORK_DIR,"temp"),"models")
-PREDICT_DIR=os.path.join(os.path.join(WORK_DIR,"images"),"predict")
-IMAGE_DIM=224
-
-MODEL=load_model(os.path.join(MODEL_DIR,"model.hd5"))
-test_image = image.load_img(os.path.join(os.path.join(PREDICT_DIR,'a'),'photo.jpg'), target_size=(IMAGE_DIM, IMAGE_DIM))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
-
-img=keras.applications.mobilenet.preprocess_input(test_image)
-
-predictions=MODEL.predict(img)
-print(predictions)
-predictions=MODEL.predict(img)
-print(predictions)
-predictions=MODEL.predict(img)
-print(predictions)
+home = os.path.dirname(os.path.abspath(__file__))
+for root, dirs, files in os.walk(os.path.join(home,'d')):
+    for file in files:
+        if file.endswith('.jpg'):
+                path = os.path.join(root, file)
+                img = cv2.imread(os.path.join(home, path))
+                both_img = cv2.flip(img, +1)
+                cv2.imwrite(path, both_img)
